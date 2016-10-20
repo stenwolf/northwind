63cf2fa491673db3d73e765264778ad1ab15b4c8
Anh Ho <anhphanho@gmail.com>
2016-10-20 12:14:14 -0700

    product tests

--- app/tests/products.server.model.test.js
+++ app/tests/products.server.model.test.js
@@ -5,51 +5,13 @@
  */
 var should = require('should'),
 	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
 	Products = mongoose.model('Products');
 
 /**
  * Globals
  */
-var user, products;
+var products;
 
 /*
  * Unit tests
  */
-describe('Products Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
-
-		user.save(function() { 
-			products = new Products({
-				// Add model fields
-				// ...
-			});
-
-			done();
-		});
-	});
-
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return products.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
-	});
-
-	afterEach(function(done) { 
-		Products.remove().exec();
-		User.remove().exec();
-
-		done();
-	});
-});
